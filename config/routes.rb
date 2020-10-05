1df6e8f34a32ca6517fcf4917bc57d0d3bb30285
pquezadam <pquezada2009@hotmail.es>
2020-10-05 20:12:56 -0300

    config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     end
 
     unauthenticated :user do
-      root 'main#unauthenticated'
+      devise_scope :user do
+        root to: 'devise/sessions#new'
+      end
     end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
